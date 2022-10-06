444a22deade0920e92481ef917c03ef5246a228e
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2022-10-06 16:02:12 -0500

    updates

--- simulation/flow_boiling/build.sh
+++ simulation/flow_boiling/build.sh
@@ -4,8 +4,7 @@ cd $FLASHX_HOME
 # setup the problem
 ./setup incompFlow/FlowBoiling -auto -2d -nxb=16 -nyb=16 \
 	-maxblocks=100 -site=$FLASHX_SITE +amrex +parallelIO \
-        SimForceInOut=True
-
+        SimForceInOut=True IOWriteGridFiles=True
 
 # compile, copy and destroy object
 cd object && make -j && cp flashx $JOB_FILEDIR/flashx && cd .. && rm -rf object
--- simulation/pool_boiling/build.sh
+++ simulation/pool_boiling/build.sh
@@ -4,7 +4,7 @@ cd $FLASHX_HOME
 # setup the problem
 ./setup incompFlow/PoolBoiling -auto -2d -nxb=16 -nyb=16 \
 	-maxblocks=100 -site=$FLASHX_SITE +amrex +parallelIO \
-        SimForceInOut=True
+        SimForceInOut=True IOWriteGridFiles=True
 
 # compile, copy and destroy object
 cd object && make -j && cp flashx $JOB_FILEDIR/flashx && cd .. && rm -rf object
