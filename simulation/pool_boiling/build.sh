03dd25cc80d8a3b47448cc9ba4ae5a1dfa70a6b0
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2022-10-06 15:28:49 -0500

    update gitignore

--- simulation/pool_boiling/build.sh
+++ simulation/pool_boiling/build.sh
@@ -3,7 +3,8 @@ cd $FLASHX_HOME
 
 # setup the problem
 ./setup incompFlow/PoolBoiling -auto -2d -nxb=16 -nyb=16 \
-	-maxblocks=100 -site=$FLASHX_SITE +amrex +parallelIO
+	-maxblocks=100 -site=$FLASHX_SITE +amrex +parallelIO \
+        SimForceInOut=True
 
 # compile, copy and destroy object
 cd object && make -j && cp flashx $JOB_FILEDIR/flashx && cd .. && rm -rf object
