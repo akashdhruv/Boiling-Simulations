deacbc03a322ee2555b95cf4a73f1d8c8bdc494e
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2022-10-07 13:27:46 -0500

    update bash file to checkout main branch

--- simulation/flash.sh
+++ simulation/flash.sh
@@ -1,7 +1,11 @@
 # File for `jobrunner` setup command
 
-# Initialize Flash-X submodule
+# initialize Flash-X submodule
 git submodule update --init $FLASHX_HOME
+
+# chdir into Flash-X, and
+# checkout main branch
+cd $FLASHX_HOME && git checkout main
 
 # set Flash-X makefile PATH
 export FLASHX_SITE=$JOB_FILE_HOME
