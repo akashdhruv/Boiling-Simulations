dd3a781e036f4e70171bdba5fe03ab4c471d4d3a
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2022-10-11 14:56:24 -0500

    update interface

--- simulation/FlowBoiling/runFlash.sh
+++ simulation/FlowBoiling/runFlash.sh
@@ -1,5 +1,2 @@
-# cache the value of current directory
-NodeDir=$(realpath .)
-
 # Run the actualy job using this target script
-cd $JobWorkDir && mpirun $NodeDir/flashx -par_file job.input
+cd $JobWorkDir && mpirun job.target -par_file job.input
