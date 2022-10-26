b6375b584da0ba92bd30e1695db981e7daf71a61
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2022-10-26 15:05:51 -0500

    update HDF5 configure line

--- software/setupHDF5.sh
+++ software/setupHDF5.sh
@@ -9,7 +9,8 @@ cd HDF5 && git checkout 93754ca
 
 # configure HDF5
 ./autogen.sh
-./configure --enable-parallel --enable-fortran --prefix=$HDF5_HOME
+./configure --enable-parallel --enable-fortran CC=mpicc CXX=mpicxx \
+	FC=mpif90 FF=mpif90 F77=mpif90 --prefix=$HDF5_HOME
 
 # compile and install
 make -j && make install
