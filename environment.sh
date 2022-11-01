d6640806ce7d595772daca7cffd228e5c3108281
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2022-11-01 16:30:54 -0500

    update organization again

--- environment.sh
+++ environment.sh
@@ -9,20 +9,22 @@ JobSite="sedona"
 
 # Load modules for
 # the site directory
-source $(realpath sites/$JobSite)/modules.sh
+source $(realpath sites/$JobSite)/Modules.sh
 
-# Get mpicc path and
-# then remove /bin/mpicc 
-# to get set MPI_HOME
-MPI_HOME=$(which mpicc)
-MPI_REMOVE=/bin/mpicc
-export MPI_HOME=${MPI_HOME//$MPI_REMOVE/}
+# Set MPI_HOME by quering path 
+# loaded by site module
+export MPI_HOME=$(which mpicc | sed s/'\/bin\/mpicc'//)
 
 # Export project home
 export PROJECT_HOME=$(realpath .)
 
 # Path to parallel HDF5 installtion with fortran support
-export HDF5_HOME="$(realpath software/HDF5)/install-$JobSite"
+if [ $(which h5pfc) ]; then
+   export HDF5_HOME=$(which h5pfc | sed s/'\/bin\/h5pfc'//)
+else
+   export HDF5_HOME="$(realpath software/HDF5)/install-$JobSite"
+   BuildLocalHDF5="TRUE"
+fi
 
 # Store path to amrex as environment variable
 export AMREX2D_HOME="$(realpath software/AMReX)/install-$JobSite/2D"
@@ -33,3 +35,15 @@ export FLASH_HOME=$(realpath software/Flash-X)
 
 # Path to Flash-X site/makefile
 FlashSite=$(realpath sites/$JobSite)
+
+# Output information to stdout
+echo "----------------------------------"
+echo "Boiling Simulations Environment:"
+echo "PROJECT_HOME=$PROJECT_HOME"
+echo "MPI_HOME=$MPI_HOME"
+echo "HDF5_HOME=$HDF5_HOME"
+echo "FLASH_HOME=$FLASH_HOME"
+echo "FLASH_SITE=$FlashSite"
+echo "AMREX2D_HOME=$AMREX2D_HOME"
+echo "AMREX3D_HOME=$AMREX3D_HOME"
+echo "----------------------------------"
--- software/setupHDF5.sh
+++ software/setupHDF5.sh
@@ -1,16 +1,18 @@
 # Bash script for `jobrunner`
 # to install HDF5
 
-# Initialize amrex submodule
-git submodule update --init HDF5
+if [[ $BuildLocalHDF5 == "TRUE" ]]; then
+	# Initialize amrex submodule
+	git submodule update --init HDF5
 
-# chdir into amrex and checkout desired branch
-cd HDF5 && git checkout 93754ca
+	# chdir into amrex and checkout desired branch
+	cd HDF5 && git checkout 93754ca
 
-# configure HDF5
-./autogen.sh
-./configure --enable-parallel --enable-fortran CC=mpicc CXX=mpicxx \
-	FC=mpif90 FF=mpif90 F77=mpif90 --prefix=$HDF5_HOME
+	# configure HDF5
+	./autogen.sh
+	./configure --enable-parallel --enable-fortran CC=mpicc CXX=mpicxx \
+		FC=mpif90 FF=mpif90 F77=mpif90 --prefix=$HDF5_HOME
 
-# compile and install
-make -j && make install
+	# compile and install
+	make -j && make install
+fi
